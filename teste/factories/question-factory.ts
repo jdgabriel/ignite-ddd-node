db27318e2ffe22dd4ca3c74d092c3488630c3f62
Gabriel Duarte <jds.gabrielduarte@gmail.com>
2023-11-09 15:27:41 -0300

    test: intall faker lib

--- teste/factories/question-factory.ts
+++ teste/factories/question-factory.ts
@@ -1,16 +1,22 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
-import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
 
-export function questionFactory(override?: Partial<QuestionProps>) {
-  return Question.create({
-    authorId: new UniqueEntityID('1'),
-    title: 'Question title',
-    slug: Slug.create('question-title'),
-    content: 'Question content',
-    ...override,
-  })
+export function questionFactory(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  return Question.create(
+    {
+      authorId: new UniqueEntityID('1'),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 }
